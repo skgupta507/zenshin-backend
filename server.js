85042715dfbf6b9e9d7648595ee1e0d6652c3b1e
Sunil Gupta <sunilk98850@gmail.com>
2024-12-13 11:16:47 +0530

    "Refactored Code"

--- server.js
+++ server.js
@@ -5,16 +5,14 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 import chalk from "chalk";
+import { exec } from "child_process";
 
 // Get the current directory
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// const tempDir = path.join(process.env.TEMP || "/tmp", "myapp_subtitles");
-// const webtDir = path.join(process.env.TEMP || "/tmp", "webtorrent");
-// console.log(tempDir);
+// Ensure the downloads directory exists
 const downloadsDir = path.join(__dirname, "downloads");
-console.log(downloadsDir);
 if (!fs.existsSync(downloadsDir)) {
   fs.mkdirSync(downloadsDir, { recursive: true });
 }
@@ -23,11 +21,6 @@ const app = express();
 const client = new WebTorrent();
 
 app.use(cors());
-
-// Ensure the temporary directory exists
-// if (!fs.existsSync(tempDir)) {
-//   fs.mkdirSync(tempDir, { recursive: true });
-// }
 
 /* ------------- CHECK LATEST GITHUB RELEASE ------------ */
 const owner = "hitarth-gg"; // Replace with the repository owner
@@ -63,7 +56,6 @@ getLatestRelease();
 /* ------------------------------------------------------ */
 
 /* ----------------- SEED EXISTING FILES ---------------- */
-// Seed all existing files on server startup
 const seedExistingFiles = () => {
   fs.readdir(downloadsDir, (err, files) => {
     if (err) {
@@ -76,8 +68,6 @@ const seedExistingFiles = () => {
 
       if (fs.lstatSync(filePath).isFile()) {
         client.seed(filePath, { path: downloadsDir }, (torrent) => {
-          // console.log(`Seeding file: ${filePath}`);
-          // console.log(`Magnet URI: ${torrent.magnetURI}`);
           console.log(
             chalk.bgBlue("Seeding started: "),
             chalk.cyan(torrent.name)
@@ -90,279 +80,85 @@ const seedExistingFiles = () => {
     });
   });
 };
-
-// Call the function to start seeding existing files
 seedExistingFiles();
 /* ------------------------------------------------------ */
 
+/* ----------------------- ROUTES ----------------------- */
+
+// Add a torrent
 app.get("/add/:magnet", async (req, res) => {
   let magnet = req.params.magnet;
-
-  /* ------------------------------------------------------ */
-  // Check if the torrent is already added
-  let existingTorrent = await client.get(magnet);
-  console.log("Existing torrent:", existingTorrent);
+  let existingTorrent = client.get(magnet);
 
   if (existingTorrent) {
-    // If torrent is already added, return its file information
     let files = existingTorrent.files.map((file) => ({
       name: file.name,
       length: file.length,
     }));
-    // console.log("Existing torrent files:", files);
-
     return res.status(200).json(files);
   }
-  /* ------------------------------------------------------ */
 
-  client.add(magnet, function (torrent) {
+  client.add(magnet, (torrent) => {
     let files = torrent.files.map((file) => ({
       name: file.name,
       length: file.length,
     }));
-    // console.log(files);
-
     res.status(200).json(files);
   });
 });
 
-/* -------------------- GET METADATA -------------------- */
-app.get("/metadata/:magnet", async (req, res) => {
-  let magnet = req.params.magnet;
+// Stream a file from a torrent
+app.get("/streamfile/:magnet/:filename", async (req, res) => {
+  let { magnet, filename } = req.params;
+  let tor = client.get(magnet);
 
-  /* ------------------------------------------------------ */
-  // Check if the torrent is already added
-  let existingTorrent = await client.get(magnet);
-  console.log("Existing torrent:", existingTorrent);
-
-  if (existingTorrent) {
-    // If torrent is already added, return its file information
-    let files = existingTorrent.files.map((file) => ({
-      name: file.name,
-      length: file.length,
-    }));
-    // console.log("Existing torrent files:", files);
-
-    return res.status(200).json(files);
-  }
-  /* ------------------------------------------------------ */
-
-  const torrent = client.add(magnet, { deselect: true, path: downloadsDir });
-
-  torrent.on("metadata", () => {
-    const files = torrent.files.map((file) => ({
-      name: file.name,
-      length: file.length,
-    }));
-    console.log(files);
-
-    res.status(200).json(files);
-  });
-});
-
-app.get("/streamfile/:magnet/:filename", async function (req, res, next) {
-  let magnet = req.params.magnet;
-  let filename = req.params.filename;
-
-  console.log(magnet);
-
-  let tor = await client.get(magnet);
-
-  if (!tor) {
-    return res.status(404).send("Torrent not found");
-  }
-
+  if (!tor) return res.status(404).send("Torrent not found");
   let file = tor.files.find((f) => f.name === filename);
-  console.log("file :" + file.toString());
 
-  if (!file) {
-    return res.status(404).send("No file found in the torrent");
-  }
-  console.log(file);
-
+  if (!file) return res.status(404).send("No file found in the torrent");
   file.select();
 
   let range = req.headers.range;
-
-  console.log("Range : " + range);
-
-  if (!range) {
-    return res.status(416).send("Range is required");
-  }
+  if (!range) return res.status(416).send("Range is required");
 
   let positions = range.replace(/bytes=/, "").split("-");
   let start = parseInt(positions[0], 10);
-  let file_size = file.length;
-  let end = positions[1] ? parseInt(positions[1], 10) : file_size - 1;
-  let chunksize = end - start + 1;
+  let fileSize = file.length;
+  let end = positions[1] ? parseInt(positions[1], 10) : fileSize - 1;
+  let chunkSize = end - start + 1;
 
-  let head = {
-    "Content-Range": `bytes ${start}-${end}/${file_size}`,
+  res.writeHead(206, {
+    "Content-Range": `bytes ${start}-${end}/${fileSize}`,
     "Accept-Ranges": "bytes",
-    "Content-Length": chunksize,
+    "Content-Length": chunkSize,
     "Content-Type": "video/x-matroska",
-  };
-
-  res.writeHead(206, head);
-
-  let stream_position = {
-    start: start,
-    end: end,
-  };
-
-  detailsOfEpisode.percentageWatched = (start / end) * 100;
-  console.log(detailsOfEpisode);
-  
-
-  let stream = file.createReadStream(stream_position);
-  stream.pipe(res);
-
-  stream.on("error", function (err) {
-    console.error("Stream error:", err);
-    // Only send a response if headers haven't been sent yet
-    if (!res.headersSent) {
-      return res.status(500).send("Error streaming the video");
-    }
   });
 
-  stream.on("close", () => {
-    console.log("Stream closed prematurely");
-  });
+  file.createReadStream({ start, end }).pipe(res);
 });
 
-// Deselect an episode with the given filename
+// Deselect a file
 app.get("/deselect/:magnet/:filename", async (req, res) => {
-  let magnet = req.params.magnet;
-  let filename = req.params.filename;
+  let { magnet, filename } = req.params;
+  let tor = client.get(magnet);
 
-  let tor = await client.get(magnet);
-
-  if (!tor) {
-    return res.status(404).send("Torrent not found");
-  }
-
+  if (!tor) return res.status(404).send("Torrent not found");
   let file = tor.files.find((f) => f.name === filename);
 
-  if (!file) {
-    return res.status(404).send("No file found in the torrent");
-  }
-
-  console.log(chalk.bgRed("Download Stopped:") + " " + chalk.cyan(file.name));
+  if (!file) return res.status(404).send("No file found in the torrent");
 
   file.deselect();
-
   res.status(200).send("File deselected successfully");
 });
 
-// get download details of a file
-
-let detailsOfEpisode = {
-  name: "",
-  length: 0,
-  downloaded: 0,
-  progress: 0,
-  percentageWatched: 0,
-}
-
-app.get("/detailsepisode/:magnet/:filename", async (req, res) => {
-  let magnet = req.params.magnet;
-  let filename = req.params.filename;
-
-  let tor = await client.get(magnet);
-  if (!tor) {
-    return res.status(404).send("Torrent not found");
-  }
-
-  let file = tor.files.find((f) => f.name === filename);
-  if (!file) {
-    return res.status(404).send("No file found in the torrent");
-  }
-
-  // let details = {
-  detailsOfEpisode = {
-    name: file.name,
-    length: file.length,
-    downloaded: file.downloaded,
-    progress: file.progress,
-    percentageWatched: detailsOfEpisode.percentageWatched,
-  };
-
-  res.status(200).json(detailsOfEpisode);
-});
-
-/* ------------------------------------------------------ */
-
-app.get("/stream/:magnet", async function (req, res, next) {
-  let magnet = req.params.magnet;
-  console.log(magnet);
-
-  let tor = await client.get(magnet);
-
-  if (!tor) {
-    return res.status(404).send("Torrent not found");
-  }
-
-  let file = tor.files.find((f) => f.name.endsWith(".mkv"));
-  console.log("file :" + file.toString());
-
-  if (!file) {
-    return res.status(404).send("No MP4 file found in the torrent");
-  }
-
-  let range = req.headers.range;
-  console.log("Range : " + range);
-
-  if (!range) {
-    return res.status(416).send("Range is required");
-  }
-
-  let positions = range.replace(/bytes=/, "").split("-");
-  let start = parseInt(positions[0], 10);
-  let file_size = file.length;
-  let end = positions[1] ? parseInt(positions[1], 10) : file_size - 1;
-  let chunksize = end - start + 1;
-
-  let head = {
-    "Content-Range": `bytes ${start}-${end}/${file_size}`,
-    "Accept-Ranges": "bytes",
-    "Content-Length": chunksize,
-    "Content-Type": "video/x-matroska",
-  };
-
-  res.writeHead(206, head);
-
-  let stream_position = {
-    start: start,
-    end: end,
-  };
-
-  let stream = file.createReadStream(stream_position);
-  stream.pipe(res);
-
-  stream.on("error", function (err) {
-    console.error("Stream error:", err);
-    // Only send a response if headers haven't been sent yet
-    if (!res.headersSent) {
-      return res.status(500).send("Error streaming the video");
-    }
-  });
-
-  stream.on("close", () => {
-    console.log("Stream closed prematurely");
-  });
-});
-
+// Get torrent details
 app.get("/details/:magnet", async (req, res) => {
   let magnet = req.params.magnet;
+  let tor = client.get(magnet);
 
-  // Find the torrent by magnet link
-  let tor = await client.get(magnet);
-  if (!tor) {
-    return res.status(404).send("Torrent not found");
-  }
+  if (!tor) return res.status(404).send("Torrent not found");
 
-  // Prepare torrent details
-  let details = {
+  res.status(200).json({
     name: tor.name,
     length: tor.length,
     downloaded: tor.downloaded,
@@ -372,86 +168,18 @@ app.get("/details/:magnet", async (req, res) => {
     progress: tor.progress,
     ratio: tor.ratio,
     numPeers: tor.numPeers,
-  };
-
-  res.status(200).json(details);
-});
-
-/* --------------- Handling VLC streaming --------------- */
-import { exec } from "child_process";
-import { get } from "http";
-// Full path to VLC executable, change it as needed
-const vlcPath = '"C:\\Program Files (x86)\\VideoLAN\\VLC\\vlc.exe"'; // Adjust this path as needed
-
-app.get("/stream-to-vlc", async (req, res) => {
-  const { url, magnet } = req.query;
-
-  if (!url) {
-    return res.status(400).send("URL is required");
-  }
-  const vlcCommand = `${vlcPath} "${url}"`;
-
-  exec(vlcCommand, (error) => {
-    if (error) {
-      console.error(`Error launching VLC: ${error.message}`);
-      return res.status(500).send("Error launching VLC");
-    }
-    res.send("VLC launched successfully");
-  });
-});
-/* ------------------------------------------------------ */
-
-app.delete("/remove/:magnet", async (req, res) => {
-  let magnet = req.params.magnet;
-
-  // Find the torrent by magnet link
-  let tor = await client.get(magnet);
-  if (!tor) {
-    return res.status(404).send("Torrent not found");
-  }
-
-  // Destroy the torrent to stop downloading and remove it from the client
-  tor.destroy((err) => {
-    if (err) {
-      console.error("Error removing torrent:", err);
-      return res.status(500).send("Error removing torrent");
-    }
-
-    res.status(200).send("Torrent removed successfully");
   });
 });
 
-// ping backend
+// Ping the backend
 app.get("/ping", (req, res) => {
   res.status(200).send("pong");
 });
 
-// app.get("/subtitles/:magnet", (req, res) => {
-//   let magnet = req.params.magnet;
+/* ------------------------------------------------------ */
 
-//   let tor = client.get(magnet);
-//   if (!tor) {
-//     return res.status(404).send("Torrent not found");
-//   }
-
-//   // Find subtitle file in the torrent
-//   let subtitleFile = tor.files.find(f => f.name.endsWith(".srt"));
-//   if (!subtitleFile) {
-//     return res.status(404).send("No subtitle file found in the torrent");
-//   }
-
-//   res.setHeader("Content-Type", "text/vtt");
-//   let stream = subtitleFile.createReadStream();
-//   stream.pipe(res);
-
-//   stream.on("error", function (err) {
-//     console.error("Subtitle stream error:", err);
-//     if (!res.headersSent) {
-//       return res.status(500).send("Error streaming the subtitle file");
-//     }
-//   });
-// });
-
-app.listen(64621, () => {
-  console.log("Server running at http://localhost:64621");
+// Use the Vercel default port or fallback to 3000
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
